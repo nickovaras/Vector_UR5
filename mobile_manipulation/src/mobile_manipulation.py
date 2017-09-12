8c9f25fa1d7fd274566d9946af8240e35b60dede
nickovaras <nicovaras@gmail.com>
2017-09-11 20:16:49 -0400

    Update mobile_manipulation.py

--- mobile_manipulation/src/mobile_manipulation.py
+++ mobile_manipulation/src/mobile_manipulation.py
@@ -5,6 +5,8 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 
+from srdfdom.srdf import SRDF
+
 from math import sin, cos
 
 from geometry_msgs.msg import PoseStamped
@@ -31,35 +33,30 @@ class MoveBaseClient(object):
         self.client.send_goal(move_goal)
         self.client.wait_for_result()
 
+def fetch_joint_angles_from_srdf(state_name):
+    # Create a dictionary of group_states
+    statedict = {}
+    for x in range(len(robot.group_states)):
+        statedict[robot.group_states[x].name]=x
+
+    group_state_joint_angles=[]
+    for x in range(6):
+    	group_state_joint_angles.append(float(str(robot.group_states[statedict[state_name]].joints[x].value)[1:-1]))
+    return group_state_joint_angles
+
 
 
 if __name__ == "__main__":
     # Create a node
     rospy.init_node("mobmanip")
 
+    #Parse the SRDF from the parameter server
+    robot = SRDF.from_parameter_server()
+
     # start moveit part
     group = moveit_commander.MoveGroupCommander("manipulator")
     group.set_planner_id("RRTConnectkConfigDefault")
     display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
-    
-
-        # <group_state name="extended_up" group="manipulator">
-        #     <joint name="ur5_arm_elbow_joint" value="0" />
-        #     <joint name="ur5_arm_shoulder_lift_joint" value="-1.5531" />
-        #     <joint name="ur5_arm_shoulder_pan_joint" value="0" />
-        #     <joint name="ur5_arm_wrist_1_joint" value="-1.6237" />
-        #     <joint name="ur5_arm_wrist_2_joint" value="3.1063" />
-        #     <joint name="ur5_arm_wrist_3_joint" value="0" />
-        # </group_state>
-
-        # <group_state name="cobra" group="manipulator">
-        #     <joint name="ur5_arm_elbow_joint" value="1.4825" />
-        #     <joint name="ur5_arm_shoulder_lift_joint" value="-2.1179" />
-        #     <joint name="ur5_arm_shoulder_pan_joint" value="1.7649" />
-        #     <joint name="ur5_arm_wrist_1_joint" value="-2.1885" />
-        #     <joint name="ur5_arm_wrist_2_joint" value="-1.48" />
-        #     <joint name="ur5_arm_wrist_3_joint" value="-1.6237" />
-        # </group_state>
 
     # Make sure sim time is working
     while not rospy.Time.now():
@@ -69,28 +66,26 @@ if __name__ == "__main__":
     move_base = MoveBaseClient()
 
     while not rospy.is_shutdown():
-        # Move to first location
+        # Move Vectorto first location
         rospy.loginfo("Moving to location1...")
         move_base.goto(2.250, 3.118, 0.0)
 
         # Move arm to "extended_up" configuration
         group.clear_pose_targets()
         group_variable_values=group.get_current_joint_values()
-	group_variable_values={0,-1.5531,0,-1.6237,3.1063,0}
+    	group_variable_values=fetch_joint_angles_from_srdf("extended_up")
         group.set_joint_value_target(group_variable_values)
         extended_up = group.plan()
         group.execute(extended_up)
-	#group.go()
 
-        # Move to second location
+        # Move Vector to second location
         rospy.loginfo("Moving to location2...")
         move_base.goto(-3.53, 3.75, 1.57)
 
-	# Move arm to "cobra" configuration
+    	# Move arm to "cobra" configuration
         group.clear_pose_targets()
-	group_variable_values=group.get_current_joint_values()
-	group_variable_values={1.4825,-2.1179,1.7649,-2.1885,-1.48,-1.6237}
+    	group_variable_values=group.get_current_joint_values()
+    	group_variable_values=fetch_joint_angles_from_srdf("cobra")
         group.set_joint_value_target(group_variable_values)
         cobra = group.plan()
         group.execute(cobra)
-        #group.go()
